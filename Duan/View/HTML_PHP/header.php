2f6aec36bb2f247e3d8d33d203dcb53a50d58263
dobachinh04 <dobachinh04@gmail.com>
2023-11-19 01:41:03 +0700

    .

--- Duan/View/HTML_PHP/header.php
+++ Duan/View/HTML_PHP/header.php
@@ -10,7 +10,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <!-- custom css -->
-  <link rel="stylesheet" href="./Duan/View/CSS_JS/style2.css">
+  <link rel="stylesheet" href="./Duan/View/CSS_JS/style3.css">
 </head>
 
 <body>
