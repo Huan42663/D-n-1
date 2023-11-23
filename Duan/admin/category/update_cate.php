93eb1167409c5f3b887f629c48020100ef6e7648
quangvt12 <quangtranvinh802@gmail.com>
2023-11-23 10:32:04 +0700

    ok

--- Duan/admin/category/update_cate.php
+++ Duan/admin/category/update_cate.php
@@ -1,11 +1,14 @@
+<div class="from">
 <h2> <i class="fa-solid fa-shop"></i> Sửa Loại </h2>
+<hr>
 <div class="form-update">
     <?php
         extract($category);
     ?>
     <form action="index.php?act=updated_cate" enctype="multipart/form-data" method="POST">
         <input type="hidden" name="id_cate" value="<?=$id_cate?>">
-        tên Loại <input type="text" name="cate_name" value="<?=$cate_name?>"><br>
-        <input type="submit" name="sua" value="Sửa">
+        <h5>Tên Loại <input type="text" name="cate_name" value="<?=$cate_name?>" class="name"></h5><br><br>
+        <input type="submit" name="sua" value="Sửa" class="button">
     </form>
+</div>
 </div>
